c5383084cda73e6356d3a4e6819c4aeed7d72129
Luka Popovic <lukapopovic0111@gmail.com>
2025-03-27 16:05:27 +0100

    Change the color of the button

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 const btnOne = document.querySelector('.btn-1');
 const btnTwo = document.querySelector('.btn-2');
 const btnThree = document.querySelector('.btn-3');
+btnOne.addEventListener('click', function () {
+  btnOne.style.backgroundColor = 'rgba(99, 237, 99, 0.849)';
+  btnOne.style.color = 'white';
+});
+btnTwo.addEventListener('click', function () {
+  btnTwo.style.backgroundColor = 'rgba(99, 237, 99, 0.849)';
+  btnTwo.style.color = 'white';
+});
+btnThree.addEventListener('click', function () {
+  btnThree.style.backgroundColor = 'rgba(99, 237, 99, 0.849)';
+  btnThree.style.color = 'white';
+});
